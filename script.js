00cf545b0940c3aeca2a41f79139503bf1234d3f
jai40021210 <33617047-jai40021210@users.noreply.replit.com>
2024-04-25 02:42:51 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,78 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
+
+// Display Mobile Menu
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
+
+// Show active menu when scrolling
+const highlightMenu = () => {
+  const elem = document.querySelector('.highlight');
+  const homeMenu = document.querySelector('#home-page');
+  const aboutMenu = document.querySelector('#about-page');
+  const servicesMenu = document.querySelector('#services-page');
+  let scrollPos = window.scrollY;
+  // console.log(scrollPos);
+
+  // adds 'highlight' class to my menu items
+  if (window.innerWidth > 960 && scrollPos < 600) {
+    homeMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 1400) {
+    aboutMenu.classList.add('highlight');
+    homeMenu.classList.remove('highlight');
+    servicesMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 2345) {
+    servicesMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+  }
+
+  if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+    elem.classList.remove('highlight');
+  }
+};
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
+
+//  Close mobile Menu when clicking on a menu item
+const hideMobileMenu = () => {
+  const menuBars = document.querySelector('.is-active');
+  if (window.innerWidth <= 768 && menuBars) {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.remove('active');
+  }
+};
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
+const output = document.querySelector(".output");
+const ranges = document.querySelectorAll("input[type=range]");
+const colorCode = document.querySelector(".colorCode");
+updateColor();
+ranges.forEach((range) => range.addEventListener("input", updateColor));
+
+function updateColor() {
+const color = ` rgb(${ranges[0].value}, ${ranges[1].value}, ${ranges[2].value}) `;
+output.style.backgroundColor = color;
+colorCode.innerText = color;
+}
+
+colorCode.addEventListener("click", function () {
+navigator.clipboard
+.writeText(colorCode.innerText)
+.then(() => {
+  alert("add to clipboard");
+})
+.catch(() => {
+  alert("something went worng");
+});
+});
